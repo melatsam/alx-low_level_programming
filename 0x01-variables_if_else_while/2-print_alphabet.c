dc36e023c86b6f2f12a3fb20b1b32761cf177037
melatsam <88444516+melatsam@users.noreply.github.com>
2021-08-29 01:58:33 -0700

    play with alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
 
 /**
-* main - prints out all the numbers between 00 and 99
-* with no two digits being the same
+* main - prints out the alphabet in lowercase
 * Return: 0
 */
 int main(void)
 {
-int i, j;
+char a = 'a';
 
-for (i = 48; i < 58; i++)
+while (a <= 'z')
 {
-for (j = i; j < 58; j++)
-{
-if (i == j)
-{
-continue;
+putchar(a);
+a++;
 }
-
-putchar(i);
-putchar(j);
-
-if (i == 56 && j == 57)
-{
-break;
-}
-else
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
+  
 putchar('\n');
 return (0);
 }
