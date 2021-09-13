ccf7095804ad5f98ce4cc20ead205d8fb86b52e4
melatsam <88444516+melatsam@users.noreply.github.com>
2021-09-13 14:27:17 -0700

    more pointers

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
 *set_string - function that sets the value of a pointer to a char
 *
@@ -9,7 +8,6 @@
 *
 *Return: void
 */
-
 void set_string(char **s, char *to)
 {
 *s = to;
