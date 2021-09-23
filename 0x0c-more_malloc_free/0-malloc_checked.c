455166eebf60795ec119b940e399f9df81c50496
root <88444516+melatsam@users.noreply.github.com>
2021-09-23 11:51:34 -0700

    malloc

--- 0x0c-more_malloc_free/0-malloc_checked.c
+++ 0x0c-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,21 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-*malloc_checked - allocates memory using malloc
-*@b: number of bytes to be malloced
-*Return: returns a void pointer
+**malloc_checked - allocates memory using malloc
+*@b: integer
+*Return: pointer
 */
+
 void *malloc_checked(unsigned int b)
 {
-void *ptr;
+int *ptr;
 
 ptr = malloc(b);
-if (!ptr)
+if (ptr == NULL)
+{
 exit(98);
+}
 return (ptr);
 }
